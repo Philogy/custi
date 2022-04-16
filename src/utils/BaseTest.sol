1f09f35cd4aa8644830da1c0860263490a0f1f11
philogy <philippe@dumo.net>
2022-04-17 00:47:28 +0200

    Add utility to base test

--- src/utils/BaseTest.sol
+++ src/utils/BaseTest.sol
@@ -9,7 +9,18 @@ contract BaseTest is DSTest {
     CheatCodes internal cheats = CheatCodes(HEVM_ADDRESS);
 
     function _advanceTime(uint256 _increase) internal returns (uint256 newTime) {
+        // solhint-disable-next-line not-rely-on-time
         newTime = block.timestamp + _increase;
         cheats.warp(newTime);
+    }
+
+    function _transferNative(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal returns (bool success, bytes memory returndata) {
+        cheats.prank(_from);
+        // solhint-disable-next-line avoid-low-level-calls
+        (success, returndata) = _to.call{value: _amount}("");
     }
 }

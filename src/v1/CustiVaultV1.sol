7eadba5924dee5fc250b38d45d44771d620acaae
Philippe Dumonet <philippe@dumo.net>
2022-09-08 20:15:40 +0200

    Add view methods, silence unnecessary warnings

--- src/v1/CustiVaultV1.sol
+++ src/v1/CustiVaultV1.sol
@@ -16,26 +16,31 @@ contract CustiVaultV1 is ICustiVaultV1 {
     uint256 internal constant NOT_PING_MASK = ~(uint256(type(uint48).max) << 160);
 
     modifier onlyOwner() {
-        uint256 curSlot0 = slot0;
-        _ownerLockCheck(curSlot0);
-        slot0 = _ping(curSlot0);
+        uint256 slot0_ = slot0;
+        _checkOwner(slot0);
+        _checkLock(slot0);
+        slot0 = _ping(slot0_);
         _;
     }
 
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+
     function lockTill(uint256 _timestamp) external {
-        uint256 curSlot0 = slot0;
-        _checkOwner(curSlot0);
-        slot0 = _setLockTill(curSlot0, _timestamp);
+        uint256 slot0_ = slot0;
+        _checkOwner(slot0_);
+        slot0 = _setLockTill(slot0_, _timestamp);
     }
 
     function lockFor(uint256 _lockDuration) external {
-        uint256 curSlot0 = slot0;
-        _checkOwner(curSlot0);
-        slot0 = _setLockTill(curSlot0, block.timestamp + _lockDuration);
+        uint256 slot0_ = slot0;
+        _checkOwner(slot0_);
+        // solhint-disable-next-line not-rely-on-time
+        slot0 = _setLockTill(slot0_, block.timestamp + _lockDuration);
     }
 
     // solhint-disable-next-line no-empty-blocks
-    function ping() external onlyOwner {}
+    function ping() external payable onlyOwner {}
 
     function doCustomCall(
         address _target,
@@ -48,8 +53,35 @@ contract CustiVaultV1 is ICustiVaultV1 {
         if (_requireSuccess && !success) revert CustomCallFailed(returndata);
     }
 
-    function _ownerLockCheck(uint256 _slot0) internal view {
-        _checkOwner(_slot0);
+    function ownerPingLockedTill()
+        public
+        view
+        returns (
+            address owner_,
+            uint256 lastPing_,
+            uint256 lockedTill_
+        )
+    {
+        uint256 slot0_ = slot0;
+        owner_ = address(uint160(slot0_));
+        lastPing_ = uint48(slot0_ >> PING_OFFSET);
+        lockedTill_ = slot0_ >> LOCKED_TILL_OFFSET;
+    }
+
+    function owner() public view returns (address) {
+        return address(uint160(slot0));
+    }
+
+    function lastPing() public view returns (uint256) {
+        return uint48(slot0 >> PING_OFFSET);
+    }
+
+    function lockedTill() public view returns (uint256) {
+        return slot0 >> LOCKED_TILL_OFFSET;
+    }
+
+    function _checkLock(uint256 _slot0) internal view {
+        // solhint-disable-next-line not-rely-on-time
         if ((_slot0 >> LOCKED_TILL_OFFSET) > block.timestamp) revert UseWhileLocked();
     }
 
@@ -64,6 +96,7 @@ contract CustiVaultV1 is ICustiVaultV1 {
         uint256 currentLock = _slot0 >> LOCKED_TILL_OFFSET;
         if (_timestamp <= currentLock) revert InvalidLockTime();
         emit Locked(_timestamp);
+        // solhint-disable-next-line not-rely-on-time
         if (currentLock > block.timestamp) {
             // Vault still locked, no ping
             return (_slot0 & OWNER_PING_MASK) | (_timestamp << LOCKED_TILL_OFFSET);
@@ -72,6 +105,7 @@ contract CustiVaultV1 is ICustiVaultV1 {
             emit Ping();
             return
                 (_slot0 & OWNER_MASK) |
+                // solhint-disable-next-line not-rely-on-time
                 (block.timestamp << PING_OFFSET) |
                 (_timestamp << LOCKED_TILL_OFFSET);
         }

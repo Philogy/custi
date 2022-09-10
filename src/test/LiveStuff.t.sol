16716e290aa0219ed3563050834f27f9ca87a08e
Philippe Dumonet <philippe@dumo.net>
2022-09-10 10:56:37 +0200

    Improve custom call env

--- src/test/LiveStuff.t.sol
+++ src/test/LiveStuff.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.13;
 
 import {BaseTest} from "../utils/BaseTest.sol";
 import {IAssetVault} from "../IAssetVault.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface IStorerBase {
     function target() external view returns (address);
@@ -10,6 +11,10 @@ interface IStorerBase {
     function show() external view returns (bool);
 
     function requireSuccess() external view returns (bool);
+
+    function totalCalls() external view returns (uint256);
+
+    function increase() external;
 }
 
 struct Call {
@@ -17,9 +22,10 @@ struct Call {
     bytes data;
     uint256 value;
     bool requireSuccess;
+    uint256 index;
 }
 
-contract CallStorer is BaseTest {
+contract CallStorer {
     IStorerBase private immutable storerBase;
 
     constructor(address _storerBase) {
@@ -37,59 +43,93 @@ contract CallStorer is BaseTest {
                 target: storerBase.target(),
                 data: _calldata,
                 value: msg.value,
-                requireSuccess: storerBase.requireSuccess()
+                requireSuccess: storerBase.requireSuccess(),
+                index: storerBase.totalCalls()
             })
         );
+        storerBase.increase();
+        return "";
+    }
+}
+
+struct VaultCall {
+    bytes data;
+    uint256 index;
+}
+
+contract VaultCallStorer {
+    IStorerBase private immutable storerBase;
+
+    constructor(address _storerBase) {
+        storerBase = IStorerBase(_storerBase);
+    }
+
+    VaultCall[] private calls;
+
+    fallback(bytes calldata _calldata) external payable returns (bytes memory) {
+        if (storerBase.show()) {
+            return abi.encode(calls);
+        }
+        calls.push(VaultCall({data: _calldata, index: storerBase.totalCalls()}));
+        storerBase.increase();
         return "";
     }
 }
 
 /// @author Philippe Dumonet
 contract LiveStuffBase is BaseTest, IStorerBase {
-    address public override target;
-    bool public override show;
-    bool public override requireSuccess;
+    address public target;
+    bool public show;
+    bool public requireSuccess;
+    uint256 public totalCalls;
 
     address internal callStorer;
+    IAssetVault internal vault;
 
     function setUp() public {
         cheats.deal(msg.sender, 1e9 ether);
     }
 
+    function increase() external {
+        totalCalls++;
+    }
+
     function _resetCalls() internal {
         callStorer = address(new CallStorer(address(this)));
+        vault = IAssetVault(address(new VaultCallStorer(address(this))));
+        totalCalls = 0;
     }
 
     modifier showCalls() {
         _resetCalls();
         _;
         show = true;
-        (, bytes memory returndata) = callStorer.call("");
+        (, bytes memory returndataCalls) = callStorer.call("");
+        (, bytes memory returndataVaultCalls) = address(vault).call("");
         show = false;
-        Call[] memory calls = abi.decode(returndata, (Call[]));
+        Call[] memory calls = abi.decode(returndataCalls, (Call[]));
+        VaultCall[] memory vaultCalls = abi.decode(returndataVaultCalls, (VaultCall[]));
 
-        emit log("calls:\n");
-
-        uint256 callLen = calls.length;
-        if (callLen == 1) {
-            emit log("doCustomCall");
-            emit log_named_address("_target", calls[0].target);
-            emit log_named_uint("_value", calls[0].value);
-            emit log_named_bytes("_calldata", calls[0].data);
-            emit log_named_string("_requireSuccess", calls[0].requireSuccess ? "true" : "false");
-        } else {
-            emit log("multicall");
-            emit log("data:\n");
-            for (uint256 i = 0; i < callLen; i++) {
-                Call memory call = calls[i];
+        emit log("multicall");
+        emit log("data:\n");
+        uint256 totalCalls = calls.length + vaultCalls.length;
+        uint256 callIndex = 0;
+        uint256 vaultCallIndex = 0;
+        for (uint256 i = 0; i < totalCalls; i++) {
+            if (callIndex < calls.length && calls[callIndex].index == i) {
+                Call memory call = calls[callIndex];
                 emit log_bytes(
                     abi.encodeCall(
                         IAssetVault.doCustomCall,
                         (call.target, call.value, call.data, call.requireSuccess)
                     )
                 );
-                emit log(",\n");
+                callIndex++;
+            } else {
+                emit log_bytes(vaultCalls[vaultCallIndex].data);
+                vaultCallIndex++;
             }
+            emit log(",\n");
         }
     }
 }
@@ -103,10 +143,19 @@ interface IWeth {
 contract LiveStuff is LiveStuffBase {
     function testSolution() public showCalls {
         requireSuccess = true;
-        target = 0xae7e201257f3F7918E9e8F2F3De998E3D75f7A1d;
-        IWeth c = IWeth(callStorer);
-        c.deposit{value: 2 ether}();
+        target = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-        c.withdraw(0xae7e201257f3F7918E9e8F2F3De998E3D75f7A1d);
+        IWeth weth = IWeth(callStorer);
+
+        weth.deposit{value: 3 ether}();
+
+        for (uint256 i = 0; i < 20; i++) {
+            vault.transferNFT(
+                IERC721(0xC4638af1e01720C4B5df3Bc8D833db6be85d2211),
+                0x488F1889E5e219e5228e86e2E41f9a9c74A28397,
+                0x33CC24dbf9c8FDDB574077eE0Fa1d2b93B566381,
+                i + 4555
+            );
+        }
     }
 }
